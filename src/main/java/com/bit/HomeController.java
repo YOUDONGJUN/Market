abf75e1a9c7ed0b35e51424dd24fe96feed16da7
YOUDONGJUN <ehdwnsdb@naver.com>
2023-03-21 11:34:26 +0900

    230321 11:34

--- src/main/java/com/bit/HomeController.java
+++ src/main/java/com/bit/HomeController.java
@@ -1,16 +1,16 @@
 package com.bit;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
@@ -18,30 +18,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+//        logger.info("Welcome home! The client locale is {}.", locale);
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
-		String formattedDate = dateFormat.format(date);
+        String formattedDate = dateFormat.format(date);
 
-		model.addAttribute("serverTime", formattedDate );
+        model.addAttribute("serverTime", formattedDate);
 
-		return "index";
-	}
+        return "index";
+    }
 
-	@GetMapping("index")
-	public String index() {
-		System.out.println("index controller 실행");
-		return "index";
-	}
+    @GetMapping("index")
+    public String index() {
+        System.out.println("index controller 실행");
+        return "index";
+    }
 
 //	@GetMapping("index")
 //	public String index(@RequestParam("id") String id, HttpSession session) {
